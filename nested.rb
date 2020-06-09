b2ff97f0a39346c21e3c0fb6f126c72a3222e466
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-09 09:13:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,14 +80,10 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        },
-				:yukihiro_matsumoto => {
-				   :known_for => "Ruby",
-				    :languages => ["LISP", "C"]
-				}
+        }
      }
 
-
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 	programmer_hash
 end
 
